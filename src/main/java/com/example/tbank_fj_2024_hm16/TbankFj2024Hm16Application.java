9548c5d73548fae7cec4bc689ddf3d2d6ee42237
Danis <danisharisov@gmail.com>
2024-11-28 13:29:47 +0300

    Initial commit

---
+++ src/main/java/com/example/tbank_fj_2024_hm16/TbankFj2024Hm16Application.java
@@ -0,0 +1,13 @@
+package com.example.tbank_fj_2024_hm16;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TbankFj2024Hm16Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TbankFj2024Hm16Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tbank_fj_2024_hm16/TbankFj2024Hm16ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tbank_fj_2024_hm16;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TbankFj2024Hm16ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
